fc39b203e807ae0782d139c30cc1d5b42546ff7f
mayurinsrithong <mayurin0039@gmail.com>
2018-06-26 11:41:01 +0700

    hollo world

---
+++ src/lab1/myCode.java
@@ -0,0 +1,25 @@
+package lab1;
+
+public class myCode {
+    public static void main(String[] args) {
+
+     System.out.println("Hello World.");
+
+        System.out.println("MAYURIN SRITHONG");
+        System.out.println("361211760039");
+        System.out.println("MIT211");
+        System.out.println("RMUTSV");
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}//class
