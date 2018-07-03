2d4c7bf6e8d45779d9a5b07fa01f244371d0f2e3
email <mayurin0039@gmail.com>
2018-07-03 11:16:38 +0700

    DATA PROJACT

---
+++ src/lab2/ex1.java
@@ -0,0 +1,36 @@
+package lab2;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+        int x;
+        x = 10;
+        System.out.println(x);
+        x = 20;
+        System.out.println(x);
+        int y = 30;
+        System.out.println(y);
+        int t = x+y;
+        System.out.println(t);
+        System.out.println(y-x);
+
+
+
+
+
+
+
+
+
+
+    }//main
+
+
+
+
+
+
+
+
+}//class
+
