cab01afcb2735e231d49b0f48f0d1f6b8ea74c38
mayurinSrithong <mayurin0093@gmail.com>
2018-08-14 11:58:35 +0700

    Quiz

---
+++ src/Quiz/Quiz_1.java
@@ -0,0 +1,44 @@
+package Quiz;
+
+public class Quiz_1 {
+    public static void main(String[] args) {
+        int count = 0;
+        int total = 0;
+
+        for (int i = 500; i < 1000; i++) {
+            //statement for
+            if (i % 3 == 0 || i % 6 == 0)
+                count++;
+            total += i; //total =total +i;
+            System.out.println(i + " ");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }//main
+}//class
---
+++ src/Quiz/Quiz_3.java
@@ -0,0 +1,41 @@
+package Quiz;
+
+import java.util.Scanner;
+
+public class Quiz_3 {
+    public static void main(String[] args) {
+
+                int select;
+                System.out.println("เลือกประห้องพัก?");
+                System.out.println("Room Type 1 ");
+                System.out.println("Room Type 2 ");
+                System.out.println("Room Type 3 ");
+                System.out.println("Room Type 4 ");
+
+
+                Scanner input = new Scanner(System.in);
+                System.out.print("เลือกประเภทห้องพัก(1-4):");
+                select = input.nextInt();
+                //switch-case
+
+                System.out.println("จำนวนวันที่เข้าพัก");
+                select = input.nextInt();
+                //switch-case
+
+                System.out.println("ค่าห้องที่ต้องจ่าย");
+
+
+
+                switch (select){
+
+
+                }//switch-case
+
+
+
+
+
+
+    }//main
+
+}//class
